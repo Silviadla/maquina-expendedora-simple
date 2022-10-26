ae1eddf5871ee0108753f577a912a1b9cf91460f
Silvia <silviadelucasalv@gmail.com>
2022-10-26 11:08:40 +0200

    Precio y lugar fijo

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -17,11 +17,11 @@ public class MaquinaExpendedoraSimple {
      * del billete que se recibe es mayor que 0.
      */
     public MaquinaExpendedoraSimple(int precioDelBillete, String origen, String destino) {
-        precioBillete = precioDelBillete;
-        balanceClienteActual = 0;
+        precioBillete = 20;
+        
         totalDineroAcumulado = 0;
-        estacionOrigen = origen;
-        estacionDestino = destino;
+        estacionOrigen = "León";
+        estacionDestino = "Madrid";
     }
 
     /**
