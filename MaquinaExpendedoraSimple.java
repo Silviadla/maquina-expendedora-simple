985984c651cfc7a076573280a97191ba60b3d900
Silvia <silviadelucasalv@gmail.com>
2022-10-28 10:55:36 +0200

    Segundo constructor

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -16,14 +16,26 @@ public class MaquinaExpendedoraSimple {
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple(int precioDelBillete, String origen, String destino) {
+    public MaquinaExpendedoraSimple() {
         precioBillete = 20;
-        
+        balanceClienteActual = 0;
         totalDineroAcumulado = 0;
         estacionOrigen = "León";
         estacionDestino = "Madrid";
     }
-
+    
+    /**
+     * Crea una maquina expendedora de billetes de tren con el 
+     * precio del billete y el origen y destino dados. Se asume que el precio
+     * del billete que se recibe es mayor que 0.
+     */
+    public MaquinaExpendedoraSimple (int preciodelBillete, String destino) {
+        precioBillete = preciodelBillete;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "León";
+        estacionDestino = destino;
+    }
     /**
      * Devuelve el precio del billete
      */
